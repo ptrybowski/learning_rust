0c154423bcce8fd1fbc6a5e7c4198c09919b38ab
Piotr Trybowski <piotr.trybowski@boombit.com>
2021-06-07 08:43:09 +0200

    Adding files

---
+++ lib/gl/target/debug/build/gl2-1b7d8eaaeadb2641/build_script_build-1b7d8eaaeadb2641.d
@@ -0,0 +1,5 @@
+C:\Users\ADMIN\PycharmProjects\learning_rust\lib\gl2\target\debug\build\gl2-1b7d8eaaeadb2641\build_script_build-1b7d8eaaeadb2641.exe: build.rs
+
+C:\Users\ADMIN\PycharmProjects\learning_rust\lib\gl2\target\debug\build\gl2-1b7d8eaaeadb2641\build_script_build-1b7d8eaaeadb2641.d: build.rs
+
+build.rs:
---
+++ src/fizzbuzz.rs
@@ -0,0 +1,17 @@
+
+pub fn fizzbuzz(){
+    for i in 0..100 {
+        if i%15 == 0 {
+            print!("FizzBuzz");
+        }
+        else if i%3 ==0 {
+            print!("Fizz");
+        }
+        else if i%5==0 {
+            print!("Buzz");
+        }
+        else {
+            print!("{}",i);
+        }
+    }
+}
